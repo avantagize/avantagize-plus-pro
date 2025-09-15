9840e54de15ca1569a315ea321f60e78138aee62
avantagize <avantagize@gmail.com>
2025-09-16 02:57:47 +0300

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,11 +3,14 @@ import React from "react";
 export const Logo: React.FC = () => {
   return (
     <div className="flex items-center">
+      {/* Logo image */}
       <img
-        src="/logo.png"   // your new purple logo in public/
+        src="/logo.png"   // make sure your logo.png is in public/ or assets/
         alt="Avantagize Plus"
         className="h-8 w-auto"
       />
+
+      {/* Brand name */}
       <span className="ml-2 font-semibold text-lg text-gray-900">
         Avantagize Plus
       </span>
