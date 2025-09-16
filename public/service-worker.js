5b8726b44de47fb29cf1795baf12fe51f469b039
avantagize <avantagize@gmail.com>
2025-09-16 03:45:09 +0300

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "avantagize-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/logo-192x192.png",
+  "/logo-512x512.png"
+];
+
+// Install SW and cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update cache when new version available
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
+  );
+});
